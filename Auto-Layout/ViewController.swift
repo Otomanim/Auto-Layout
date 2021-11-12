1ae3ffdf4849cf39a7db196b7ddf063e26ce12c0
Evandro Minamoto <89124140+Otomanim@users.noreply.github.com>
2021-11-12 19:26:17 -0300

    Metodo
    implementei um método para  reutilizar nas views

--- Auto-Layout/ViewController.swift
+++ Auto-Layout/ViewController.swift
@@ -8,56 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let redView = UIView()
         redView.backgroundColor = .red
         
-        let blueView = UIView()
-        blueView.backgroundColor = .blue
+        //        let blueView = UIView()
+        //        blueView.backgroundColor = .blue
+        //
+        //        let yellowView = UIView()
+        //        yellowView.backgroundColor = .yellow
         
-        let yellowView = UIView()
-        yellowView.backgroundColor = .yellow
+        view.addSubview(redView)
         
-//        view.addSubview(redView)
-//        view.addSubview(blueView)
-//
-//        redView.translatesAutoresizingMaskIntoConstraints = false
-//        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
-//
-//        blueView.translatesAutoresizingMaskIntoConstraints = false
-//        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//        blueView.leadingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
-//        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//MARK: -
-//        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        redView.preencher(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: view.bottomAnchor,
+            padding: .init(top: 50, left: 30, bottom: 100, right: 50))
         
-//        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //        view.addSubview(blueView)
+        //
+        //        redView.translatesAutoresizingMaskIntoConstraints = false
+        //        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        //        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        //        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        //        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+        //
+        //        blueView.translatesAutoresizingMaskIntoConstraints = false
+        //        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //        blueView.leadingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
+        //        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        //        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        //MARK: -
+        //        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        //        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
         
-        let horizontalStackView = UIStackView(arrangedSubviews: [redView, blueView])
-        horizontalStackView.distribution = .fillEqually
+        //        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
-        let stackView: UIStackView = UIStackView(arrangedSubviews: [horizontalStackView, yellowView])
-        stackView.distribution = .fillEqually
-        stackView.axis = .vertical
-        
-        view.addSubview(stackView)
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        //        let horizontalStackView = UIStackView(arrangedSubviews: [redView, blueView])
+        //        horizontalStackView.distribution = .fillEqually
+        //
+        //        let stackView: UIStackView = UIStackView(arrangedSubviews: [horizontalStackView, yellowView])
+        //        stackView.distribution = .fillEqually
+        //        stackView.axis = .vertical
+        //
+        //        view.addSubview(stackView)
+        //
+        //        stackView.translatesAutoresizingMaskIntoConstraints = false
+        //        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        //        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        //        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
-
-
+    
+    
 }
 
